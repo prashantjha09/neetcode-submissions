325bb0a917ec8f9424dbee3cdd649a57c0d069b4
Prashant <41165408+prashantjha09@users.noreply.github.com>
2026-04-26 21:30:56 +0530

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        pair_dict = { "}":"{", "]":"[", ")":"(" }
+        stack = deque()
+        for i in s:
+            if i in pair_dict.values():
+                print(f"into if {i}")
+                stack.append(i)
+            else:
+                if not stack:
+                    return False
+                val = stack.pop()
+                if val != pair_dict[i]:
+                    return False
+        return True if not stack else False
+        
